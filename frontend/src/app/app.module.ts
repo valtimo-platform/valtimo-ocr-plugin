7656952953705f4855d95042298fa0490dd6f1df
Klaas Schuijtemaker <klaas.schuijtemaker@ritense.com>
2026-04-21 12:25:46 +0200

    Replace sample-plugin references with valtimo-ocr

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -75,7 +75,7 @@ import {TeamsModule} from "@valtimo/teams";
 import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
 import {environment} from "../environments/environment";
 
-import {SamplePluginModule, samplePluginSpecification,} from "@valtimo-plugins/sample-plugin";
+import {SamplePluginModule, samplePluginSpecification,} from "@valtimo-plugins/valtimo-ocr";
 
 export function tabsFactory() {
   return new Map<string, object>([
